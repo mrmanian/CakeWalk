e21ba9169d8867cffc52c372cbf13d6942f9238b
EC2 Default User <ec2-user@ip-172-31-72-75.ec2.internal>
2020-10-13 19:35:22 +0000

    Handle errors from Heroku deployment

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ socketio.init_app(app, cors_allowed_origins="*")
 dotenv_path = join(dirname(__file__), 'sql.env')
 load_dotenv(dotenv_path)
 
-sql_user = os.environ['SQL_USER']
-sql_pwd = os.environ['SQL_PASSWORD']
+
 
 database_uri = os.environ('DATABASE_URL') 
 
