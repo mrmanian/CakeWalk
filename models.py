8cd25c27a56adf653e64d0072bd3c8a843c0e94b
Devin Romanoff <ec2-user@ip-172-31-60-93.ec2.internal>
2020-11-06 18:38:37 +0000

    Updated models.py with project and task tables

--- models.py
+++ models.py
@@ -17,3 +17,37 @@ class users(db.Model):
         
     def __repr__(self):
        return '< %i : %s : %d : %s>' % (self.profile_img, self.username, self.email, self.group_code)
+       
+class Projects(db.Model):
+    proj_id = db.Column(db.Integer, primary_key=True)
+    group_code = db.Column(db.Integer)
+    proj_name = db.Column(db.String(500))
+    p_description = db.Column(db.String(1000))
+    
+    def __init__(self, pi, gc, pn, d):
+        self.proj_id = pi
+        self.group_code = gc
+        self.proj_name = pn
+        self.p_description = d
+        
+    def __repr__(self):
+        return '< %i : %d : %d >' % (self.group_code, self.proj_name, self.description)
+    
+class Tasks(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    proj_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    title = db.Column(db.String(255))
+    t_description = db.Column(db.String(1000))
+    date = db.Column(db.String(255))
+    
+    def __init__(self, ti, pi, ui, t, td, d):
+        self.task_id = ti
+        self.proj_id = pi
+        self.user_id = ui
+        self.title = t
+        self.t_description = td
+        self.date = d
+        
+    def __repr__(self):
+        return '< %s : %s : %s : %s >' % (self.title, self.title, self.t_description, self.date)
