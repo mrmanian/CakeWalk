289d3ac7040ff6629e6708747c437fb43baecbcb
mrmanian <mikemanian@gmail.com>
2020-11-19 21:22:39 +0000

    Change path from Login to LoginPage

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dash from './Dash';
-import Login from './Login';
+import LoginPage from './LoginPage';
 import { Socket } from './Socket';
-/* eslint import/no-extraneous-dependencies: */
+
 export default function Content() {
-  const [login, setLogin] = React.useState(false);
-  const [email, setEmail] = React.useState('');
+  const [login, setLogin] = useState(false);
+  const [email, setEmail] = useState('');
 
   function updateLogin(data) {
-    /* eslint no-console: ["error", { allow: ["log"] }] */
     console.log(`Received login status from server: ${data.loginStatus}`);
     setLogin(data.loginStatus);
     setEmail(data.email);
   }
   function getLogin() {
-    React.useEffect(() => {
+    useEffect(() => {
       Socket.on('login_status', updateLogin);
       return () => {
         Socket.off('login_status', updateLogin);
@@ -27,5 +26,5 @@ export default function Content() {
     return (<Dash email={email} />);
   }
 
-  return (<Login />);
+  return (<LoginPage />);
 }
