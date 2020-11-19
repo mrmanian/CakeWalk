d74c5dcbb25be8718985d6405714e6dda366658d
mrmanian <mikemanian@gmail.com>
2020-11-19 22:16:09 +0000

    Add facebook login and add method to forgot password button

--- scripts/Login.jsx
+++ scripts/Login.jsx
@@ -4,6 +4,15 @@ import FacebookButton from './FacebookButton';
 import './Login-Register.css';
 
 export default function Login() {
+  function handleClick() {
+    if (document.getElementById('Email').value === '') {
+      alert('Enter valid email address!');
+    } else {
+      console.log(document.getElementById('Email').value);
+      document.getElementById('Email').value = '';
+    }
+  }
+
   return (
     <div className="box-container">
       <div className="head">Login</div>
@@ -19,11 +28,12 @@ export default function Login() {
           <input type="password" id="Password" name="Password" placeholder="Password" autoComplete="on" required />
           <button type="submit" className="login-button">Login</button>
         </form>
-        <p className="forgot">Forgot Password</p>
+        <p className="forgot" onClick={handleClick} onKeyDown={handleClick}>Forgot Password</p>
         <hr />
         <p className="or">Or continue with your social account</p>
         <div className="socials">
           <GoogleButton />
+          <FacebookButton />
         </div>
       </div>
     </div>
