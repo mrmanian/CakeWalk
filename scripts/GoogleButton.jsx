eb6509faf5c91baa20252dd802844e5f965db43e
mrmanian <mikemanian@gmail.com>
2020-11-19 21:20:09 +0000

    Move google login button to its own file

---
+++ scripts/GoogleButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { GoogleLoginButton } from 'react-social-login-buttons';
+import { Socket } from './Socket';
+
+export default function GoogleButton() {
+  function handleSubmit(response) {
+    const { name } = response.profileObj;
+    const { email } = response.profileObj;
+    const picUrl = response.profileObj.imageUrl;
+    Socket.emit('newlogin', { uname: name, imageurl: picUrl, email });
+  }
+
+  function handleFailure() {
+    return (null);
+  }
+
+  return (
+    <GoogleLogin
+      clientId="589283421664-6kvonrml8a1lod7mhtmucru816l5bv20.apps.googleusercontent.com"
+      render={(renderProps) => (
+        <GoogleLoginButton onClick={renderProps.onClick} disabled={renderProps.disabled} />
+      )}
+      onSuccess={handleSubmit}
+      onFailure={handleFailure}
+      cookiePolicy="single_host_origin"
+    />
+  );
+}
