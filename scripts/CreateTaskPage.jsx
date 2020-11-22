0b2efe98f7ffcef37292bc2cb76ca9d7f9acdef2
mrmanian <mikemanian@gmail.com>
2020-11-22 04:44:22 +0000

    Add feature where form must be filled in order to submit to prevent null values

--- scripts/CreateTaskPage.jsx
+++ scripts/CreateTaskPage.jsx
@@ -1,10 +1,8 @@
-/* eslint import/no-extraneous-dependencies: */
 import React, { useState } from 'react';
 import { Socket } from './Socket';
 import Dash from './Dash';
 import './CreateTaskPage.css';
 
-/* eslint-disable react/prop-types */
 export default function CreateTaskPage({ email }) {
   const [titleValue, updateTitleValue] = useState('');
   const [descriptionValue, updateDescriptionValue] = useState('');
@@ -12,12 +10,11 @@ export default function CreateTaskPage({ email }) {
   const [formSent, setFormSent] = useState(false);
   const [cancel, setCancel] = useState(false);
 
-  // Gathers submitted information and sends to server
-  function handleSubmit(event) {
+  function handleSubmit() {
     updateTitleValue('');
-    event.preventDefault();
     updateDescriptionValue('');
     updateDeadlineValue('');
+
     Socket.emit('create task', {
       email,
       title: titleValue,
@@ -27,17 +24,15 @@ export default function CreateTaskPage({ email }) {
     setFormSent(true);
   }
 
-  // Redirect page back to dashboard after form submit
   if (formSent) {
     return (<Dash email={email} />);
   }
-  
-  function handleCancel(event){
-    console.log("Hit Cancel");
+
+  function handleCancel() {
     setCancel(true);
   }
-  
-  if(cancel){
+
+  if (cancel) {
     return (<Dash email={email} />);
   }
 
@@ -48,15 +43,15 @@ export default function CreateTaskPage({ email }) {
       </h1>
       <form onSubmit={handleSubmit} autoComplete="off">
         <br />
-        <textarea className="textarea" id="name" placeholder="Task Name" value={titleValue} onChange={(e) => updateTitleValue(e.target.value)} />
+        <textarea className="textarea" id="name" placeholder="Task Name" value={titleValue} onChange={(e) => updateTitleValue(e.target.value)} required />
         <br />
         <br />
-        <textarea className="textarea" id="description" placeholder="Task Description" value={descriptionValue} onChange={(e) => updateDescriptionValue(e.target.value)} />
+        <textarea className="textarea" id="description" placeholder="Task Description" value={descriptionValue} onChange={(e) => updateDescriptionValue(e.target.value)} required />
         <br />
         <label htmlFor="Deadline" className="label">
           Deadline:
           {' '}
-          <input type="date" id="deadline" name="Deadline" value={deadlineValue} onChange={(e) => updateDeadlineValue(e.target.value)} />
+          <input type="date" id="deadline" name="Deadline" value={deadlineValue} onChange={(e) => updateDeadlineValue(e.target.value)} required />
         </label>
         <button id="submit" type="submit">Create</button>
         <button id="submit" type="button" onClick={handleCancel}>Cancel</button>
