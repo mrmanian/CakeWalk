4e3883752b6e94f3105d73fd9c78bf11d1523263
mrmanian <mikemanian@gmail.com>
2020-11-22 04:28:27 +0000

    Add new column for storing password on user table

--- models.py
+++ models.py
@@ -5,20 +5,23 @@ class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
     profile_img = db.Column(db.String(500))
     group_code = db.Column(db.String(10))
 
-    def __init__(self, u, e, i, gc):
+    def __init__(self, u, e, p, i, gc):
         self.username = u
         self.email = e
+        self.password = p
         self.profile_img = i
         self.group_code = gc
 
     def __repr__(self):
-        return "< %s : %s : %s : %s >" % (
+        return "< %s : %s : %s : %s : %s >" % (
             self.profile_img,
             self.username,
             self.email,
+            self.password,
             self.group_code,
         )
 
@@ -58,10 +61,13 @@ class Tasks(db.Model):
         self.group_code = gc
         self.task_owner = to
         self.complete_status = cs
-        
+
     def __repr__(self):
-        return "< %s : %s : %s >" % (
+        return "< %s : %s : %s : %s : %s : %s >" % (
             self.title,
             self.t_description,
             self.date,
+            self.group_code,
+            self.task_owner,
+            self.complete_status,
         )
