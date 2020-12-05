ccb75e632993438810701339a01632b0ad13623b
Devin Romanoff <ec2-user@ip-172-31-62-91.ec2.internal>
2020-12-05 04:42:15 +0000

    Added drop down to select project when creating a task

--- scripts/CreateTaskPage.jsx
+++ scripts/CreateTaskPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Socket } from './Socket';
 import Dash from './Dash';
 import './CreateTaskPage.css';
@@ -9,6 +9,24 @@ export default function CreateTaskPage({ email }) {
   const [deadlineValue, updateDeadlineValue] = useState('');
   const [formSent, setFormSent] = useState(false);
   const [cancel, setCancel] = useState(false);
+  const [projects, setProjects] = useState([]);
+  
+  useEffect(() => {
+    Socket.emit('emit', {
+      email,
+    });
+  }, []);
+  
+  useEffect(() => {
+    Socket.on('proj list', (data) => {
+      setProjects(data.projs);
+    });
+    return () => {
+      Socket.off('proj list', (data) => {
+        setProjects(data.projs);
+      });
+    };
+  }, []);
 
   function handleSubmit() {
     updateTitleValue('');
@@ -35,6 +53,9 @@ export default function CreateTaskPage({ email }) {
   if (cancel) {
     return (<Dash email={email} />);
   }
+  
+  console.log('--------');
+  console.log({projects});
 
   return (
     <div id="form">
@@ -47,6 +68,14 @@ export default function CreateTaskPage({ email }) {
         <br />
         <br />
         <textarea className="textarea" id="description" placeholder="Task Description" value={descriptionValue} onChange={(e) => updateDescriptionValue(e.target.value)} required />
+        <label>Select project:</label>
+        <select name="cars" id="cars">
+        {
+          projects.map((project, index) => (
+            <option value={project[0]} key={index}>{project[0]}</option>
+          ))
+        }
+        </select>
         <br />
         <label htmlFor="Deadline" className="label">
           Deadline:
