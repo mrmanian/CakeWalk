68e64815e8a3e4056e187a1370cbdbdb37076004
mrmanian <mikemanian@gmail.com>
2020-12-07 16:57:46 +0000

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,12 @@ module.exports = {
       }
     ],
     "no-console": ["error", { allow: ["log"] }],
+    "no-alert": 0,
     "react/prop-types": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "react/no-array-index-key": 0,
+    "jsx-a11y/control-has-associated-label": 0,
+    "react/destructuring-assignment": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
   },
 };
